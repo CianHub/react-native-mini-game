f9fac0c27a97eaba4896173d89e587ea032ef34d
Cian <ihatecian@gmail.com>
2022-08-08 15:35:34 +0100

    restructure custom button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -3,18 +3,21 @@ import { View, Text, StyleSheet, Pressable } from 'react-native';
 
 export const PrimaryButton = ({ children, pressHandler }) => {
   return (
-    <Pressable onPress={pressHandler}>
-      <View style={styles.container}>
+    <View style={styles.buttonOuter}>
+      <Pressable
+        style={styles.container}
+        onPress={pressHandler}
+        android_ripple={{ color: '#640233' }}
+      >
         <Text style={styles.buttonText}>{children}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#72063c',
-    borderRadius: 28,
     paddingVertical: 8,
     paddingHorizontal: 16,
     elevation: 2,
@@ -22,10 +25,10 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
-    margin: 4,
   },
   buttonText: {
     color: 'white',
     textAlign: 'center',
   },
+  buttonOuter: { borderRadius: 28, margin: 4, overflow: 'hidden' },
 });
