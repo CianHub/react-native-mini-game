e9c1a5cb6ed3ad0ff26fcc2981297165efe626e7
Cian <ihatecian@gmail.com>
2022-08-08 15:05:31 +0100

    implement start game screen component

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -2,5 +2,9 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 export const PrimaryButton = ({ textValue }) => {
-  return <View>{textValue}</View>;
+  return (
+    <View>
+      <Text>{textValue}</Text>
+    </View>
+  );
 };
