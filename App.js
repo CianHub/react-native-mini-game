0385a69ea122c45a0b6d2f133736570eb33533a6
Cian <ihatecian@gmail.com>
2022-08-08 16:02:12 +0100

    blend image

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
-import { StyleSheet, Text } from 'react-native';
+import { ImageBackground, StyleSheet, Text } from 'react-native';
 import { StartGameScreen } from './screens/StartGameScreen';
 import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <LinearGradient style={styles.rootScreen} colors={['#ddb52f', '#4e0329']}>
-      <StartGameScreen />
+    <LinearGradient style={styles.rootScreen} colors={['#ddb52f', '#3b012f']}>
+      <ImageBackground
+        style={styles.img}
+        source={require('./assets/images/background.png')}
+        resizeMode={'cover'}
+        imageStyle={{ opacity: 0.15 }}
+      >
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
+    flex: 1,
+  },
+  img: {
     flex: 1,
   },
 });
