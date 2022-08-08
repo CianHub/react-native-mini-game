777e7fb5df0dd8fbf1c18d371c813c93591f38d5
Cian <ihatecian@gmail.com>
2022-08-08 15:53:49 +0100

    create linear gradient for home screen

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { StartGameScreen } from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
+    <LinearGradient style={styles.rootScreen} colors={['#ddb52f', '#4e0329']}>
       <StartGameScreen />
-    </View>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: '#ddb52f',
     flex: 1,
   },
 });
