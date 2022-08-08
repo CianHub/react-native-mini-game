ed62bd61d81cc0df200c8700f3f9d8104f79b152
Cian <ihatecian@gmail.com>
2022-08-08 15:21:32 +0100

    style text input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,14 +1,18 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { TextInput, View, StyleSheet } from 'react-native';
 import { PrimaryButton } from '../components/PrimaryButton';
 
 export const StartGameScreen = () => {
   return (
-    <View style={styles.container}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <TextInput style={styles.input} />
+        <PrimaryButton>Reset</PrimaryButton>
+        <PrimaryButton>Confirm</PrimaryButton>
+      </View>
+    </>
   );
 };
 
@@ -24,5 +28,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  input: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 1,
+    marginVertical: 8,
+    color: '#ddb52f',
+    fontWeight: 'bold',
+    width: 50,
+    textAlign: 'center',
   },
 });
