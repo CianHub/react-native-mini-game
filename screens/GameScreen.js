cb9902df5a9523c100fa84fbfa08a8ef2cbd1c9e
Cian <ihatecian@gmail.com>
2022-08-09 09:53:59 +0100

    scaffold game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,6 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 export const GameScreen = () => {
-  return <Text>GameScreen</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Opponent Guess</Text>
+      <View>
+        <Text>Higher or Lower?</Text>
+        <Text>Higher</Text>
+        <Text>Lower</Text>
+      </View>
+      <View>Log Rounds</View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {},
+});
