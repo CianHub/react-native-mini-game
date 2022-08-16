e4c3ae1ad97241af502ba669889d3bacea92a975
Cian <ihatecian@gmail.com>
2022-08-16 09:43:08 +0100

    update font styles

--- components/UI/Title.js
+++ components/UI/Title.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
-import { Colors } from '../../helpers/colors';
 
 export const Title = ({ title }) => {
   return <Text style={styles.title}>{title}</Text>;
@@ -8,12 +7,12 @@ export const Title = ({ title }) => {
 
 const styles = StyleSheet.create({
   title: {
-    fontWeight: 'bold',
     fontSize: 18,
     textAlign: 'center',
     color: 'white',
     borderWidth: 2,
     borderColor: 'white',
     padding: 12,
+    fontFamily: 'open-sans-bold',
   },
 });
