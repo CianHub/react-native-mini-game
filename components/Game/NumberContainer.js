cca735829c9c93672a8ce5e735b1a6bd9fd5a7c2
Cian <ihatecian@gmail.com>
2022-08-24 09:45:19 +0100

    add dimesions api

--- components/Game/NumberContainer.js
+++ components/Game/NumberContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Colors } from '../../helpers/colors';
 
 export const NumberContainer = ({ children }) => {
@@ -10,19 +10,23 @@ export const NumberContainer = ({ children }) => {
   );
 };
 
+const deviceWidth = Dimensions.get('window').width;
+
+const space = deviceWidth < 380 ? 12 : 24;
+
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: 24,
+    padding: space,
+    margin: space,
     borderRadius: 8,
-    margin: 24,
     alignItems: 'center',
     justifyContent: 'center',
   },
   numberText: {
     color: Colors.accent500,
-    fontSize: 36,
+    fontSize: deviceWidth < 380 ? 24 : 36,
     fontFamily: 'open-sans-bold',
   },
 });
